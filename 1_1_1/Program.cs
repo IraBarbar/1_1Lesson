6b05b3badb2bafa237c37d4094dad554915534a6
IraBarbar <bro998877@yahoo.com>
2022-10-13 11:08:33 +0300

    1 part

---
+++ 1_1_1/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Hello, World!");
+Console.WriteLine("My friend");
+
+int a = 25;
+double b = 1.7; 
+string c = "word";
+char d = '!';
+bool e = false;
+
+Console.WriteLine("My number is " + a + b);
+Console.WriteLine($"Hello, {a} World!");
+
+Console.Write("Imput a number: ");
+int num  = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Your number is " + num);
+
+// Задача 1. Напришите программу, которая...
+
+Console.Write("Imput a first number: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.Write(Imput the second number: );
+int n2 = Convert.ToInt32(Console.ReadLine());
