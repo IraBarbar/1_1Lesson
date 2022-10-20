eddd1330c3d6543c87979bc3c3ca4774b2b38038
IraBarbar <bro998877@yahoo.com>
2022-10-20 15:18:45 +0300

    lesson 1_3_1

---
+++ 1_2_1/Program.cs
@@ -0,0 +1,66 @@
+﻿/*
+int CutNumber(int number)
+{
+    int ed = number % 10;
+    int so = number /100;
+
+    number = so * 10 + ed; // так как имя намбе уже нигде не используется
+    return number; // cтрого однго типа
+
+}
+
+
+
+int num = new Random().Next(100, 1000); // последнее не включается.
+
+int result = CutNumber(num);
+
+Console.WriteLine($"New version of a number {num} is {result}");
+
+*/
+/*
+int FindMax(int N)
+{
+    int ed = N % 10;
+    int dec = N / 10;
+    if( ed > dec) return ed;
+    else return dec;
+}
+int n = new Random().Next(10, 100);
+int result = FindMax(n);
+
+Console.WriteLine($"In number {n} {result} is the biggest.");
+*/
+/*
+bool MultN(int n1, int n2)
+{
+ if ( n2 % n1 == 0) return true;
+ else return false;
+}
+Console.WriteLine("Please, enter the number1");
+int n1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Please, enter the number2");
+int n2 = Convert.ToInt32(Console.ReadLine());
+
+bool result = MultN(n1, n2);
+Console.WriteLine($" Do is the number {n2} to the number {n1} multiple? - {result}.");
+*/
+
+//Задач 3. Напишите программу , которая
+// принимает на входе два числа  и проверяет является ли одно число квадратом второго.
+
+ bool SquNum(int n1, int n2)
+ {
+    if(n1 * n1 == n2 || n2 * n2 == n1) 
+       return true;
+    else
+       return false; 
+ }
+ Console.WriteLine("Please, enter the number1");
+ int n1 = Convert.ToInt32(Console.ReadLine());
+
+  Console.WriteLine("Please, enter the number2");
+ int n2 = Convert.ToInt32(Console.ReadLine());
+  bool result = SquNum(n1, n2);
+  Console.WriteLine(result);
---
+++ 1_3_1/Program.cs
@@ -0,0 +1,70 @@
+﻿/*
+void ShowRange( int quart)
+{
+    if (quart == 1)
+        Console.WriteLine("x > 0 and y > 0");
+    else if (quart == 2)
+        Console.WriteLine("x < 0 and y > 0");
+    else if (quart == 3)
+        Console.WriteLine("x < 0 and y < 0");
+    else if (quart == 4)
+        Console.WriteLine("x > 0 and y < 0");  
+    else 
+        Console.WriteLine("Wrong number");         
+}
+
+Console.WriteLine("Input a number of quart");
+int quartNum = Convert.ToInt32(Console.ReadLine());
+
+ShowRange(quartNum);
+*/
+
+// Задача 2 . Напишите программу, которая принимает на входе координатные точки
+// (X, Y) причем X и Y  не равно 0 и выдает номер четверти плоскости , в которой находится эта точка.
+
+/*
+int ShowRange(int x, int y)
+{
+    if (x > 0 && y > 0) return 1;
+    else if (x < 0 && y > 0) return 2;
+    else if (x < 0 && y < 0) return 3;
+    else if (x > 0 && y < 0) return 4;
+        else return 0;
+}
+
+Console.WriteLine("Input a number X of quart");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input a number Y of quart");
+int y = Convert.ToInt32(Console.ReadLine());
+
+int resalt = ShowRange( x, y);
+Console.WriteLine(resalt);
+*/
+
+// Задача 3 . Напишите программу, которая принимает на вход координаты 2 точек и находит расстояние между 
+// ними в 2д пространстве.
+
+double FindLength(double x1, double y1, double x2, double y2)
+{
+    double a = (x2 - x1);
+    double b = (y2 - y1);
+    double c = Math.Sqrt(a*a + b*b);
+           return c;   
+}
+Console.WriteLine("Input a number X1 of quart");
+int x1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input a number Y of quart");
+int y1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input a number X2 of quart");
+int x2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input a number Y2 of quart");
+int y2 = Convert.ToInt32(Console.ReadLine());
+
+double resalt = Math.Round((FindLength(x1, y1, x2, y2)), 2);
+
+
+Console.WriteLine(resalt);
